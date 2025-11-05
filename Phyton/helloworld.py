01339f6df4fb679c96acc63d681cfce285788de6
Ekyhy <eksayahya92@gmail.com>
2025-11-05 23:59:15 +0700

    Challenges D 1

---
+++ Phyton/helloworld.py
@@ -0,0 +1,17 @@
+# Day - 1 30DaysOfPython Challenges
+print(9 + 1)
+print(5 - 2)
+print(4 * 5)
+print(4 ** 2)
+print(3 % 2)
+print(5 // 2)
+
+#Checking data types
+print(type(11))
+print(type(3.14))
+print(type(1+4j))
+print(type('Blogger'))
+print(type([1,2,3,4]))
+print(type({'name':'Blogger'}))
+print(type({9.8,3.14,2.4}))
+print(type((9.8, 3.14, 2.7))) 
