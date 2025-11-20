0ec451c5035600de7f4b2a456762d407bf665a9d
Ekyhy <eksayahya92@gmail.com>
2025-11-20 09:55:40 +0700

    Challenges Day 4: Strings Concatenation

---
+++ Phyton/strings.py
@@ -0,0 +1,26 @@
+# ========== CREATING A STRING ================
+letter = 'p'        # A srings could be a single character
+print(letter)       # P
+print(len(letter))  # 1
+
+greeting = 'Hello, World!' # String could be made using a single ('...') or double quote ("....")
+print(greeting)
+print(len(greeting)) 
+
+sentence = 'I hope you are enjoying for 30 day of Python Programming Challenges'
+print(sentence)
+print(len(sentence))
+
+
+# ====================== STRINGS CONCATENATION ===========================
+
+first_name = 'Robert'
+last_name = 'Victor'
+space = ' '
+full_name = first_name + space + last_name
+print(full_name)
+
+print(len(first_name))
+print(len(last_name))
+print(len(first_name) > len(last_name))
+print(len(full_name))
